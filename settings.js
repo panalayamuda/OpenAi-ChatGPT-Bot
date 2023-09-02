4b1555efc145e0a96f6ecc0558d83d1bd962f242
panalayamuda <143900976+panalayamuda@users.noreply.github.com>
2023-09-02 23:35:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,8 +27,8 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-global.AI_PASSWORD = ''
-global.SESSION_ID = ''
+global.AI_PASSWORD = 'king-nima-gpt-bot'
+global.SESSION_ID = 'elisa_md@;;;dcsk2IQS#ZIJuaF1mYUxppGjbVs7HLhIu3SUdZPshV3ZcK-HryuE'
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
